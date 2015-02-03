7820a5ee36640464079f557089f0a71f73260e7d
TrevorRice <trevorrr.rice@gmail.com>
2015-02-02 21:09:45 -0600

    Fixed sticky title while resizing browser and removed canvas

--- js/calendar.js
+++ js/calendar.js
@@ -3,9 +3,11 @@ var canvas;
 var ctx;
 var screenH;
 var screenW;
-var stars = [];
-var fps = 50;
-var numStars = 1000;
+var titleOffset;
+var titleOffsetHeight;
+// var stars = [];
+// var fps = 50;
+// var numStars = 1000;
 
 //jQuery(document)
 $('document').ready(function($){
@@ -62,6 +64,9 @@ $('document').ready(function($){
 
         largeHeader = document.getElementById('intro');
         largeHeader.style.height = height+'px';
+
+        titleOffset = document.getElementById('main-title').offsetTop - (document.getElementById('main-title').offsetHeight/2);
+        titleOffsetHeight = document.getElementById('main-title').offsetHeight;
     }
 
     function addListeners() {
@@ -69,101 +74,96 @@ $('document').ready(function($){
         window.addEventListener('resize', resize);
     }
 
-    var titleOffset = document.getElementById('main-title').offsetTop - (document.getElementById('main-title').offsetHeight/2);
-
-    var titleOffsetHeight = document.getElementById('main-title').offsetHeight;
-
-    function scrollCheck() {
-
-          if( window.pageYOffset > titleOffset && document.getElementById('main-title').className!='main-title sticky' ){          
-              document.getElementById('main-title').className='main-title sticky';          
-          } 
-
-          if( window.pageYOffset < titleOffset && document.getElementById('main-title').className!='main-title' ){
-              document.getElementById('main-title').className='main-title';
-          }
-
-          if( window.pageYOffset + titleOffsetHeight >   document.getElementsByClassName('calendar-wrap')[0].offsetTop ){
-              document.getElementById('main-title').className='main-title anchor';
-          }
-
-      }
-
     function resize() {
         width = window.innerWidth;
         height = window.innerHeight;
         largeHeader.style.height = height+'px';
+
+        titleOffset = document.getElementById('main-title').offsetTop - (document.getElementById('main-title').offsetHeight/2);
+        titleOffsetHeight = document.getElementById('main-title').offsetHeight;
         // canvas.width = width;
         // canvas.height = height;
     }
 
+    function scrollCheck() {
+          if( window.pageYOffset > titleOffset && document.getElementById('main-title').className!='main-title sticky' ){          
+              document.getElementById('main-title').className='main-title sticky';          
+          } 
+          if( window.pageYOffset < titleOffset && document.getElementById('main-title').className!='main-title' ){
+              document.getElementById('main-title').className='main-title';
+          }
+          if( window.pageYOffset + titleOffsetHeight > document.getElementsByClassName('calendar-wrap')[0].offsetTop ){
+              document.getElementById('main-title').className='main-title anchor';
+          }
+      }
+
     // Start creating stars to draw on canvas
-    screenH = $(window).height();
-    screenW = $(window).width();
+    // screenH = $(window).height();
+    // screenW = $(window).width();
 
-    canvas = document.getElementById('intro-canvas');
-    canvas.height = screenH;
-    canvas.width = screenW;
-    ctx = canvas.getContext('2d');
+    // canvas = document.getElementById('intro-canvas');
+    // canvas.height = screenH;
+    // canvas.width = screenW;
+    // ctx = canvas.getContext('2d');
 
-    for(var i = 0; i < numStars; i++) {
-        var x = Math.round(Math.random() * screenW);
-        var y = Math.round(Math.random() * screenH);
-        var radius = Math.random() * 3;
-        var opacity = Math.random();
+    // for(var i = 0; i < numStars; i++) {
+    //     var x = Math.round(Math.random() * screenW);
+    //     var y = Math.round(Math.random() * screenH);
+    //     var radius = Math.random() * 3;
+    //     var opacity = Math.random();
 
-        var star = new Star(x, y, radius, opacity);
+    //     var star = new Star(x, y, radius, opacity);
 
-        stars.push(star);
-    }
+    //     stars.push(star);
+    // }
 
-    setInterval(animate, 1000/fps);
+    // setInterval(animate, 1000/fps);
 });
 
 // Draw and animate stars
-function animate() {
-    ctx.clearRect(0, 0, screenW, screenH);
-    $.each(stars, function() {
-        this.draw(ctx);
-    })
-}
+// function animate() {
+//     ctx.clearRect(0, 0, screenW, screenH);
+//     $.each(stars, function() {
+//         this.draw(ctx);
+//     })
+// }
 
-function Star(x, y, radius, opacity) {
-    this.x = parseInt(x);
-    this.y = parseInt(y);
-    this.radius = radius;
-    this.opacity = opacity;
-    this.factor = 1;
-    this.increment = Math.random() * .03;
-}
+// function Star(x, y, radius, opacity) {
+//     this.x = parseInt(x);
+//     this.y = parseInt(y);
+//     this.radius = radius;
+//     this.opacity = opacity;
+//     this.factor = 1;
+//     this.increment = Math.random() * .03;
+// }
 
-Star.prototype.draw = function() {
-    ctx.rotate((Math.PI * 1 / 10));
-    ctx.save();
-    ctx.translate(this.x, this.y);
+// Star.prototype.draw = function() {
+//     ctx.rotate((Math.PI * 1 / 10));
+//     ctx.save();
+//     ctx.translate(this.x, this.y);
   
-    if(this.opacity > 1) {
-        this.factor = -1;
-    }
-    else if(this.opacity <= 0) {
-        this.factor = 1;
-        this.x = Math.round(Math.random() * screenW);
-        this.y = Math.round(Math.random() * screenH);
-    }
+//     if(this.opacity > 1) {
+//         this.factor = -1;
+//     }
+//     else if(this.opacity <= 0) {
+//         this.factor = 1;
+//         this.x = Math.round(Math.random() * screenW);
+//         this.y = Math.round(Math.random() * screenH);
+//     }
 
-    this.opacity += this.increment * this.factor;
+//     this.opacity += this.increment * this.factor;
     
-    ctx.beginPath();
+//     ctx.beginPath();
 
-    var grd = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, 3 * this.radius);
-    grd.addColorStop(0, "rgba(255,255,255," + this.opacity +")");
-    grd.addColorStop(1, "rgba(255,255,200," + this.opacity +")");
+//     var grd = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, 3 * this.radius);
+//     grd.addColorStop(0, "rgba(255,255,255," + this.opacity +")");
+//     grd.addColorStop(1, "rgba(255,255,200," + this.opacity +")");
 
-    ctx.arc(this.x,this.y,this.radius, Math.PI * 2,false);
-    ctx.fillStyle = grd;
-    ctx.shadowBlur = 50;
-    ctx.shadowColor = '#ffff33';
-    ctx.fill();
-    ctx.restore();
-}
+//     ctx.arc(this.x,this.y,this.radius, Math.PI * 2,false);
+//     ctx.fillStyle = grd;
+//     ctx.shadowBlur = 50;
+//     ctx.shadowColor = '#ffff33';
+//     ctx.fill();
+//     ctx.restore();
+// }
 
